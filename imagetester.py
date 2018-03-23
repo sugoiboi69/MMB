3ca43747c57c616fc5337e0b044f76f068dc37dd
sugoiboi69 <37665372+sugoiboi69@users.noreply.github.com>
2018-03-23 21:27:10 +0800

    Add files via upload

--- imagetester.py
+++ imagetester.py
@@ -1,3 +1,4 @@
+#dependencies (don't delete retard)
 import time
 import discord
 from discord.ext import commands
@@ -9,57 +10,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+import sys 
 
 bot = commands.Bot(command_prefix='#')
+print ("---------------------------------------------------")
 print ("RUNNING ON DISCORD VERSION: " + discord.__version__)
+print ("---------------------------------------------------")
+
+@bot.event
+async def 
 
 @bot.event
 #initiation console message
 async def on_ready():
     print ("███████████████████████ hishi's FAT mantaro moolahmaker ███████████████████████")
-    print (" ")
     print ("███████████████████████████ running on account: " + bot.user.name + " ████████████████████████████")
-    print (" ")
     print ("███████████████████████████ with ID: " + bot.user.id + " ███████████████████████████")
-    print (" ")
     print ("███████████████████████████ licensed by: ur mom LUL ███████████████████████████████")
-    print (" ")
+    time.sleep(1)
     print ("--------------------------- BEGINNING OF CHAT LOG ---------------------------------")
-    print (" ")
-    print (" ")
+
+#log each message, searches images posted    
+
+@bot.event
+
 
 @bot.event
 async def on_message(message):
-    if len(message.content)>0:
-        print(str(message.author) + ': ' + str(message.content))
 
-    elif len(message.embeds[0])>0:
+    if len(message.embeds)>0:
+        print(str(message.author)+": | Embed Attached")
+        print("----")
         embed_list = message.embeds[0]
         embed_image = embed_list['image']
         image_url = embed_image['url']
         google_images_link = "https://www.google.com/searchbyimage?&image_url=" + str(image_url)
-        drvpath = "D:\Documents\pythy discbot"
-        driver = webdriver.Firefox(drvpath)
+        driver = webdriver.Firefox()
         driver.get(google_images_link)
         divs = driver.find_element_by_class_name("r5a77d")
         bestguess = divs.find_element_by_class_name("fKDtNb").text
-        list_of_results = str(driver.find_elements_by_link_text('Pokemon'))
+        list_of_results = driver.find_elements_by_class_name('r')
         await bot.send_message(message.channel, 'The image seems to be: ' + str(bestguess))
         await bot.send_message(message.channel, '----------------------------------------------------------------')
-        if str(list_of_results) is '[]':
-            bot.send_message(message.channel, 'No results.') 
+        await bot.send_message(message.channel,'Here are search results for the image:')
+        async def msgresults():
+            for x in list_of_results:
+                result = x.text
+                await bot.send_message(message.channel, result)
+                await bot.send_message(message.channel, '---------------------------------------------------------')
+        await msgresults()
+        driver.close()
 
-        else:
-            print(str(list_of_results))       
-
-        driver.close()    
-
-    else:
-        print(str(message.author) + ': ' + str(message.content) + " | " + "--Image Attached--")
+    elif len(message.attachments)>0:
+        print(str(message.author) + ': ' + str(message.content) + " | Image Attached")
+        print("----")
         dict_of_attachments = message.attachments[0]
         google_images_link = "https://www.google.com/searchbyimage?&image_url=" + str(dict_of_attachments['url'])
-        drvpath = "D:\Documents\pythy discbot"
-        driver = webdriver.Firefox(drvpath)
+        driver = webdriver.Firefox()
         driver.get(google_images_link)
         divs = driver.find_element_by_class_name("r5a77d")
         bestguess = divs.find_element_by_class_name("fKDtNb").text
@@ -67,7 +74,15 @@ async def on_message(message):
         await bot.send_message(message.channel, 'The image seems to be: ' + str(bestguess))
         driver.close()
 
-        
+    elif len(message.content)>0:
+        print(str(message.author) + ': ' + str(message.content))
+        if 'correctly!' in str(message.content):
+            await sys.exit(msgresults())
+        elif 'end' in str(message.content):
+            await sys.exit(msgresults())
         
 
+        
+        
+#logs into the bot account (don't ban me plz lul)
 bot.run('mmsgb69@gmail.com', 'succgod123')
